0e2d12a83a93027f4a4f389a8800b3f217abc67d
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-09-16 10:02:54 +0530

    Landing page carousel

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,61 +1,68 @@
 import React, { useState, useEffect, useRef } from "react";
 import { heroData } from "../data";
-import { AiOutlineVerticalRight, AiOutlineVerticalLeft } from "react-icons/ai";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+// import { AiOutlineVerticalRight, AiOutlineVerticalLeft } from "react-icons/ai";
 
-const featuredImages = heroData;
-let count = 0;
-let slideInterval;
+// const featuredImages = heroData;
+// let count = 0;
+// let slideInterval;
 
 const Hero = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  // const [currentIndex, setCurrentIndex] = useState(0);
   const slideRef = useRef();
-  const removeAnimation = () => {
-    slideRef.current.classList.remove("fade-anim");
-  };
-  const handleOnNextClick = () => {
-    count = (count + 1) % featuredImages.length;
-    setCurrentIndex(count);
-    slideRef.current.classList.add("fade-anim");
-  };
+  // const removeAnimation = () => {
+  //   slideRef.current.classList.remove("fade-anim");
+  // };
+  // const handleOnNextClick = () => {
+  //   count = (count + 1) % featuredImages.length;
+  //   setCurrentIndex(count);
+  //   slideRef.current.classList.add("fade-anim");
+  // };
 
-  const handleOnPrevClick = () => {
-    const productsLength = featuredImages.length;
-    console.log(productsLength);
-    count = (currentIndex + productsLength - 1) % productsLength;
-    slideRef.current.classList.add("fade-anim");
-  };
+  // const handleOnPrevClick = () => {
+  //   const productsLength = featuredImages.length;
+  //   console.log(productsLength);
+  //   count = (currentIndex + productsLength - 1) % productsLength;
+  //   slideRef.current.classList.add("fade-anim");
+  // };
 
-  const pauseSlider = () => {
-    clearInterval(slideInterval);
-  };
+  // const pauseSlider = () => {
+  //   clearInterval(slideInterval);
+  // };
 
-  useEffect(() => {
-    startSlider();
-    slideRef.current.addEventListener("animationend", removeAnimation);
-    slideRef.current.addEventListener("mouseenter", pauseSlider);
-    slideRef.current.addEventListener("mouseleave", startSlider);
-    return () => {
-      clearInterval(slideInterval);
-    };
-  });
+  // useEffect(() => {
+  //   startSlider();
+  //   slideRef.current.addEventListener("animationend", removeAnimation);
+  //   slideRef.current.addEventListener("mouseenter", pauseSlider);
+  //   slideRef.current.addEventListener("mouseleave", startSlider);
+  //   return () => {
+  //     clearInterval(slideInterval);
+  //   };
+  // });
 
-  const startSlider = () => {
-    slideInterval = setInterval(() => {
-      handleOnNextClick();
-    }, 5000);
-  };
+  // const startSlider = () => {
+  //   slideInterval = setInterval(() => {
+  //     handleOnNextClick();
+  //   }, 5000);
+  // };
   return (
     <>
       <div className="w-full">
         <div ref={slideRef} className="w-full h-3/6 relative select-none">
           <div className="aspect-w-16 aspect-h-9">
-            <img
+            <Carousel>
+              {heroData.map((item) => (
+                <img src={item.src} alt={item.alt} />
+              ))}
+            </Carousel>
+            {/* <img
               src={featuredImages[currentIndex].src}
               alt={featuredImages[currentIndex].alt}
-            />
+            /> */}
           </div>
 
-          <div className="absolute w-full top-1/2 transform -translate-y-1/2 flex justify-between items-start px-3">
+          {/* <div className="absolute w-full top-1/2 transform -translate-y-1/2 flex justify-between items-start px-3">
             <button
               className="bg-black text-white p-1 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition"
               onClick={handleOnPrevClick}
@@ -68,7 +75,7 @@ const Hero = () => {
             >
               <AiOutlineVerticalLeft size={35} />
             </button>
-          </div>
+          </div> */}
         </div>
       </div>
     </>
